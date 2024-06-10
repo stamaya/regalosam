46e3eb94a4c1096d604dc040008f4c2d56e56dea
AmayaST <66726411+AmayaST@users.noreply.github.com>
2024-06-10 19:59:22 +0200

    Ajout jeu Europe

--- image/image.js
+++ image/image.js
@@ -1,8 +1,9 @@
 var images = [
 	"../screen/cap00.jpg",
 	"../screen/cap01.jpg",
+	"../screen/a.jpg",
+	"../screen/b.jpg",
 	"../screen/cap02.jpg",
-	"../screen/cap03.jpg",
 	"../screen/cap04.jpg",
 	"../screen/cap05.jpg",
 	"../screen/cap06.jpg",
@@ -19,30 +20,16 @@ var images = [
 	"../screen/cap17.jpg",
 	"../screen/cap18.jpg",
 	"../screen/cap19.jpg",
+	"../screen/cap20.jpg",
+	"../screen/cap21.jpg",
+	"../screen/cap12.jpg",
+	"../screen/cap13.jpg",
+	"../screen/cap14.jpg",
+	"../screen/cap15.jpg",
+	"../screen/cap16.jpg",
+	"../screen/the-end.jpeg",
 ]
 
-var reponses = [
-	"Rouge",
-	"Bleu",
-	"Vert",
-	"Jaune",
-	"Violet",
-	"Baseball",
-	"Bowling",
-	"Equitation",
-	"Cricket",
-	"Triple saut",
-	"Monopoly",
-	"Cluedo",
-	"Bonne paye",
-	"Tarot",
-	"Seven wonders",
-	"London",
-	"Sydney",
-	"Philadelphie",
-	"Atlanta",
-	"Villeneuve d'Ascq"
-]
 
 
 /* Initialisation de la page*/
@@ -53,12 +40,7 @@ var reponses = [
 
 	function raz() {
 		document.getElementById("image").setAttribute("src", images[id]) ;
-		console.log(images[id]);
-		document.getElementById("solution").textContent = "Réponse";
-		document.getElementById("solution").style.fontFamily = "Police1" ;
-		document.getElementById("solution").style.fontSize = "30px" ;
-		document.getElementById("solution").style.paddingBottom = "10px" ;
-		document.getElementById("solution").style.paddingTop = "10px" ;
+		
 	}
 	function precedent() {
 		id--;
@@ -68,12 +50,4 @@ var reponses = [
 	function suivant() {
 		id++;
 		raz();
-	}
-
-	function reponse() {
-		document.getElementById("solution").textContent = reponses[id] ;
-		document.getElementById("solution").style.fontFamily = "Police1" ;
-		document.getElementById("solution").style.paddingBottom = "10px" ;
-		document.getElementById("solution").style.paddingTop = "10px" ;
-
 	}
